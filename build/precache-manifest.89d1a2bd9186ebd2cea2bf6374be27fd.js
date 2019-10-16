60c68045df890d6e192ef688af7f3eb4d858a0fa
Mike <mike@mikes-mbp.home>
2019-10-15 21:02:32 -0400

    initial commit

---
+++ build/precache-manifest.89d1a2bd9186ebd2cea2bf6374be27fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9885ebb846167fc1d4cfeebc662ad05a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6036d911f77dc0a86ff2",
+    "url": "/robofriends/static/css/main.2b88d914.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "6036d911f77dc0a86ff2",
+    "url": "/robofriends/static/js/main.e08e3d92.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
